734ff93b9f7d8d238b0847fe8c31f26edc5789f4
Sam Creasey <sam.creasey@frontify.com>
2022-11-17 14:18:18 +0100

    added comments to tsconfig and eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,6 @@
+/* DO NOT DELETE:
+This file can be modified but must remain within this repository 
+for code checks performed during the review process. */
 module.exports = {
     extends: ['@frontify/eslint-config-react'],
     settings: {
